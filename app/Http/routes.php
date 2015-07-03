36b7d417507759efd0b0d87ac22cfb59faa3ac1d
Bruno Lagoa <miroldols@gmail.com>
2015-07-03 00:08:55 -0300

    - Criando padrão pattern com parâmetros  - ROTAS - Trabalhando com parâmetros

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,6 +22,34 @@ Route::get('home', 'HomeController@index');
 Route::get('admin/categories', 'AdminCategoriesController@index');
 Route::get('admin/products', 'AdminProductsController@index');
 
+Route::pattern('id', '[0-9]+');
+
+Route::get('user/{id?}', function($id = 123){
+   if($id)
+       return "Ola $id";
+    return "Não possui ID";
+});
+
+
+
+
+
+
+
+/*
+
+Route::get('user/{id?}', function($id = 123){
+   if($id)
+       return "Ola $id";
+    return "Não possui ID";
+})->where('id','[0-9]+');
+
 Route::match(['get','post'], 'exemplo2', function(){
    return "oi";
 });
+Route::any('exemplo2', function(){
+
+});
+
+Route::put('exemplo2', 'WelcomeController@exemplo');
+*/
