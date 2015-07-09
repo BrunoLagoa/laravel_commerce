d148e30d2830973f270362f53673decb0762e0a7
BrunoLagoa <miroldols@gmail.com>
2015-07-09 19:35:20 -0300

    - Criando form de categorias  - CRUD - Criando formulárioe

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -5,7 +5,23 @@
         <h1>Create Categories</h1>
 
         {!! Form::open() !!}
-        
+
+        <div class="form-group">
+
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+            <br />
+            {!! Form::label('description', 'Description:') !!}
+            {!! Form::textarea('description', null, ['class'=>'form-control']) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::submit('Add Category', ['class'=>'btn btn-primary form-control']) !!}
+
+        </div>
+
         {!! Form::close() !!}
 
     </div>
