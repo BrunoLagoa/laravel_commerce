22853796fe2c9d47bda50de45202805d56c5a2be
BrunoLagoa <miroldols@gmail.com>
2015-07-10 12:48:01 -0300

    - Ajustando formato da moeda em price para BR em index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -22,7 +22,7 @@
                 <td>{{ $product->id }}</td>
                 <td>{{ $product->name }}</td>
                 <td>{{ $product->description }}</td>
-                <td>{{ $product->price }}</td>
+                <td>{{ number_format($product->price, 2,',','.') }}</td>
                 <td>{{ $product->featured }}</td>
                 <td>{{ $product->recommend }}</td>
                 <td>
