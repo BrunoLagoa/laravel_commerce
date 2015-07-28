b1c4868888961cf9b2c4bc2cc0b18ee717fad093
BrunoLagoa <miroldols@gmail.com>
2015-07-28 17:49:12 -0300

    - Ajustando view images.blade.php  - IMAGE UPLOAD - Listando imagens

--- resources/views/products/images.blade.php
+++ resources/views/products/images.blade.php
@@ -4,38 +4,24 @@
     <div class="container">
         <h1>Images of {{ $product->name }}</h1>
 
-        <a href="{{ route('products.create') }}" class="btn btn-success">New Product</a>
+        <a href="#" class="btn btn-success">New Image</a>
         <br />  <br />
         <table class="table">
             <tr>
                 <th>ID</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Price</th>
-                <th>Featured</th>
-                <th>Recommend</th>
-                <th>Category</th>
+                <th>Image</th>
+                <th>Extension</th>
                 <th>Action</th>
             </tr>
 
-            @foreach($products as $product)
+            @foreach($product->images as $image)
             <tr>
-                <td>{{ $product->id }}</td>
-                <td>{{ $product->name }}</td>
-                <td>{{ str_limit($product->description, $limit = 100, $end = '...') }}</td>
-                <td>{{ number_format($product->price, 2,',','.') }}</td>
-                <td>{{ $product->featured == 1 ? 'Yes' : 'No' }}</td>
-                <td>{{ $product->recommend == 1 ? 'Yes' : 'No' }}</td>
-                <td>{{ $product->category->name }}</td>
-                <td>
-                    <a href="{{ route('products.edit', ['id' => $product->id]) }}">Edit</a> |
-                    <a href="{{ route('products.destroy', ['id' => $product->id]) }}">Delete</a>
-                </td>
+                <td>{{ $image->id }}</td>
+                <td></td>
+                <td>{{ $image->extension }}</td>
             </tr>
             @endforeach
         </table>
-
-        {!! $products->render() !!}
 
     </div>
 @endsection
