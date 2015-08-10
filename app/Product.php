6624c1e2c7d325ae0d9be4f84f56a691ab36fb4f
BrunoLagoa <miroldols@gmail.com>
2015-08-10 19:35:11 -0300

    - ESTRUTURA DA LOJA - Query Scope

--- app/Product.php
+++ app/Product.php
@@ -40,4 +40,9 @@ class Product extends Model
         $tags = $this->tags->lists('name')->toArray();
         return implode(',', $tags);
     }
+
+    public function scopeFeatured($query)
+    {
+        return $query->where('featured','=',1)->limit(3);
+    }
 }
