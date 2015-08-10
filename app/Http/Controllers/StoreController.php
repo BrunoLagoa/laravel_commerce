68ace90e5d3fc8c0360c90dfb9eee37e193262b4
BrunoLagoa <miroldols@gmail.com>
2015-08-10 18:51:21 -0300

    - ESTRUTURA DA LOJA - Exibindo Produtos Recomendados

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -7,13 +7,16 @@ use Illuminate\Http\Request;
 use CodeCommerce\Http\Requests;
 use CodeCommerce\Http\Controllers\Controller;
 use CodeCommerce\Category;
+use CodeCommerce\Product;
 
 class StoreController extends Controller
 {
     public function index()
     {
+        $pFeatured = Product::where('featured','=',1)->get();
+
         $categories = Category::all();
 
-        return view('store.index', compact('categories'));
+        return view('store.index', compact('categories','pFeatured'));
     }
 }
