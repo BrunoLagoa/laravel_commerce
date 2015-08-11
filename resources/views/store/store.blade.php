5bc86d6e7bd182755071c14957aad871060b093b
BrunoLagoa <miroldols@gmail.com>
2015-08-11 18:16:18 -0300

    - ELIXIR - Instalando Elixir

--- resources/views/store/store.blade.php
+++ resources/views/store/store.blade.php
@@ -137,8 +137,8 @@
     <div class="footer-bottom">
         <div class="container">
             <div class="row">
-                <p class="pull-left">Copyright © 2013 E-Shop Inc. All rights reserved.</p>
-                <p class="pull-right">Designed by <span><a target="_blank" href="http://invoinn.com/">InvoInn</a></span></p>
+                <p class="pull-left">Copyright© 2015 E-Shop Inc. All rights reserved.</p>
+                <p class="pull-right">Designed by <span><a target="_blank" href="#">Bruno Castro</a></span></p>
             </div>
         </div>
     </div>
