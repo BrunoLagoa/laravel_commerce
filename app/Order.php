9d1e27d7ae1bc16a9d35dd1390bff48ce70bb7ce
BrunoLagoa <miroldols@gmail.com>
2015-08-18 10:35:45 -0300

    - Criando o relacionamento das tabelas no Model Order e OrderItem.php  - ORDERS - Criando relacionamentos

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function items()
+    {
+        return $this->hasMany('CodeCommerce\OrderItem');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('CodeCommerce\User');
+    }
 }
--- app/OrderItem.php
+++ app/OrderItem.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    protected $table = 'order_items';
+
+    public function order()
+    {
+        return $this->belongsTo('CodeCommerce\Order');
+    }
 }
