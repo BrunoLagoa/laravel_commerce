6d74bf0536c7c2cf7f60ed2bd62fabfc911eadf4
BrunoLagoa <miroldols@gmail.com>
2015-08-18 02:14:41 -0300

    - Ajustando cart.blade.php  - Criando aviso caso não tenha nenhum item no carrinho  - CART - Projeto 12

--- resources/views/store/cart.blade.php
+++ resources/views/store/cart.blade.php
@@ -3,57 +3,63 @@
 @section('content')
     <section id="cart_items">
         <div class="container">
-
-            <div class="table-condensed cart_info">
-                <table class="table table-condensed">
-                    <thead>
-                    <tr class="cart_menu">
-                        <td class="image">Item</td>
-                        <td class="description">Descrição</td>
-                        <td class="price">Valor</td>
-                        <td class="price">Quantidade</td>
-                        <td class="price">Total</td>
-                        <td></td>
-                    </tr>
-                    </thead>
-
-                    <tbody>
-                    @forelse($cart->all() as $k=>$item)
-                        <tr>
-                            <td class="cart_product">
-                                <a href="{{ route('store.product', ['id'=>$k]) }}">
-                                    Imegem
-                                </a>
-                            </td>
-                            <td class="cart_description">
-                                <h4><a href="{{ route('store.product', ['id'=>$k]) }}">{{ $item['name'] }}</a></h4>
-                                <p>Código: {{ $k }}</p>
-                            </td>
-                            <td class="cart_price">
-                                R$ {{ number_format($item['price'],2,",",".") }}
-                            </td>
-                            <td class="cart_quantity">
-                                {!! Form::open(['route' => ['cart.update', 'id' => $k], 'method' => 'post', 'class' => 'form-inline']) !!}
-                                <div class="form-group">
-                                    {!! Form::text('qtd', $item['qtd'], ['class' => 'form-control']) !!}
-                                </div>
-                                {!! Form::submit('Alterar Qtd', ['class' => 'btn btn-success']) !!}
-                                {!! Form::close() !!}
-                            </td>
-                            <td class="cart_total">
-                                <p class="cart_total_price">R$ {{ number_format($item['price'] * $item['qtd'],2,",",".") }}</p>
-                            </td>
-                            <td class="cart_delete">
-                                <a href="{{ route('cart.destroy', ['id' => $k]) }}" class="cart_quantity_delete">Delete</a>
-                            </td>
+            @if($cart->getTotal() == 0)
+                <div class="alert alert-warning">
+                    Nenhum produto cadastrado.
+                </div>
+            @else
+                <div class="table-condensed cart_info">
+                    <table class="table table-condensed">
+                        <thead>
+                        <tr class="cart_menu">
+                            <td class="image">Item</td>
+                            <td class="description">Descrição</td>
+                            <td class="price">Valor</td>
+                            <td class="price">Quantidade</td>
+                            <td class="price">Total</td>
+                            <td></td>
                         </tr>
-                    @empty
-                        <tr>
-                            <td class="" colspan="6">
-                                Nenhum item encontrado.
-                            </td>
-                        </tr>
-                    @endforelse
+                        </thead>
+
+                        <tbody>
+                        @forelse($cart->all() as $k=>$item)
+                            <tr>
+                                <td class="cart_product">
+                                    <a href="{{ route('store.product', ['id'=>$k]) }}">
+                                        Imegem
+                                    </a>
+                                </td>
+                                <td class="cart_description">
+                                    <h4><a href="{{ route('store.product', ['id'=>$k]) }}">{{ $item['name'] }}</a></h4>
+
+                                    <p>Código: {{ $k }}</p>
+                                </td>
+                                <td class="cart_price">
+                                    R$ {{ number_format($item['price'],2,",",".") }}
+                                </td>
+                                <td class="cart_quantity">
+                                    {!! Form::open(['route' => ['cart.update', 'id' => $k], 'method' => 'post', 'class' => 'form-inline']) !!}
+                                    <div class="form-group">
+                                        {!! Form::text('qtd', $item['qtd'], ['class' => 'form-control']) !!}
+                                    </div>
+                                    {!! Form::submit('Alterar Qtd', ['class' => 'btn btn-success']) !!}
+                                    {!! Form::close() !!}
+                                </td>
+                                <td class="cart_total">
+                                    <p class="cart_total_price">
+                                        R$ {{ number_format($item['price'] * $item['qtd'],2,",",".") }}</p>
+                                </td>
+                                <td class="cart_delete">
+                                    <a href="{{ route('cart.destroy', ['id' => $k]) }}" class="cart_quantity_delete">Delete</a>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td class="" colspan="6">
+                                    Nenhum item encontrado.
+                                </td>
+                            </tr>
+                        @endforelse
                         <tr class="cart_menu">
                             <td colspan="6">
                                 <div class="pull-right">
@@ -64,10 +70,10 @@
                                 </div>
                             </td>
                         </tr>
-                    </tbody>
-                </table>
-            </div>
-
+                        </tbody>
+                    </table>
+                </div>
+            @endif
         </div>
     </section>
 @stop
