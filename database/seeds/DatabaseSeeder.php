33eed2147a81a0dbe3a19bfb3cb5e25fe5dc261c
BrunoLagoa <miroldols@gmail.com>
2015-09-03 22:03:25 -0300

    - Configurando seed para heroku

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call('UserTableSeeder');
-        $this->call('ProductTableSeeder');
-        $this->call('CategoryTableSeeder');
+        //$this->call('ProductTableSeeder');
+        //$this->call('CategoryTableSeeder');
 
         Model::reguard();
     }
